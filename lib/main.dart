0100e3500571a77920e77c4a17d3c2143b11de16
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-03-16 23:16:07 +0530

    build.gradle in project directory gms version change to 4.3.15

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'dart:async';
 
-import 'package:fintrack/auth/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
@@ -16,6 +16,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: LoginScreen());
+        );
   }
 }
