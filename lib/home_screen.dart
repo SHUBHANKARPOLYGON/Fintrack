92a608224173675a676cfecb05e4b0a96efeb7c3
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-03-16 23:07:26 +0530

    changes in firebase

---
+++ lib/home_screen.dart
@@ -0,0 +1,40 @@
+import 'package:fintrack/auth/auth_service.dart';
+import 'package:fintrack/auth/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final auth = AuthService();
+    return Scaffold(
+      body: Align(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Welcome User",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                await auth.signout();
+                goToLogin(context);
+              },
+              child: const Text("Sign Out"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  goToLogin(BuildContext context) => Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
+      );
+}
