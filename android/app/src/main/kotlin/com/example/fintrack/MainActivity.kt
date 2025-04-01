dad30c436b7799d7b0bd2bc0ecf7122f6013d8bd
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-04-01 22:54:56 +0530

    new

--- android/app/src/main/kotlin/com/example/fintrack/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/fintrack/MainActivity.kt
@@ -1,6 +1,6 @@
-package com.example.fintrack
+package com.example.fintrack;
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity;
 
-class MainActivity: FlutterActivity() {
+public class MainActivity: FlutterActivity() {
 }
